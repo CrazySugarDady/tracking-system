e0f78b977af59f228e4a0dd8e558e91d2f1d9e65
CrazySugarDady <100338504+CrazySugarDady@users.noreply.github.com>
2022-02-25 00:25:54 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->get('/order', function (Request $request) {
+Route::middleware('auth:api')->get('/order', function (Request $request) {
     return $request->order();
 });
 
-Route::group(['middleware' => ['auth:sanctum']], function () {
 
-});
